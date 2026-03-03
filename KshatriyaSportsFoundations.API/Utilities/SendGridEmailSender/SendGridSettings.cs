11db2c013b9ab9d5afda1be5fe22c48f35776821
roh-anku <stellar12.rohit@gmail.com>
2026-03-03 12:07:19 +0530

    added email service on enquiry trigger

---
+++ KshatriyaSportsFoundations.API/Utilities/SendGridEmailSender/SendGridSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace KshatriyaSportsFoundations.API.Utilities.SendGridEmailSender
+{
+    public class SendGridSettings
+    {
+        public string ApiKey { get; set; }
+        public string FromEmail { get; set; }
+        public string FromName { get; set; }
+    }
+}
